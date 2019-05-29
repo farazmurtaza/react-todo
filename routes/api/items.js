45827496f9c4600c5fdf01a81c562167124efcf4
farazmurtaza <farazmemon.16@gmail.com>
2019-05-29 22:35:36 +0500

    backend APIs completed

---
+++ routes/api/items.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+
+const Item = require('../../models/Item');
+
+router.get('/', (req, res)=>{
+    Item.find().sort({date: -1})
+    .then(items => res.json(items))
+    .catch((error)=>{
+        console.log(error)
+    });
+});
+
+router.post('/', (req, res)=>{
+    
+    const newItem = new Item({
+        name: req.body.name
+    });
+    newItem.save().then(item => res.json(item));
+
+});
+
+router.delete('/:id', (req, res)=>{
+    
+    Item.findById(req.params.id)
+    .then(item => {
+        item.remove()
+        .then( ()=> {
+            res.json({success: true});
+        })
+        .catch((err)=>{
+            console.log(err);
+        });
+    })
+    .catch((error) => {
+        res.status(404).json({ success: false});
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,12 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+//routes
+const items = require('./routes/api/items.js');
+
+//intialize app
 const app = express();
 
 app.use(bodyParser.json());
 
 const database = require('./config/database.js').mongoURI;
 
+//MLab connection
 mongoose.connect(database, { useNewUrlParser: true})
     .then( ()=>{
         console.log("Connected to MLab");
@@ -15,6 +20,8 @@ mongoose.connect(database, { useNewUrlParser: true})
     .catch( (error)=>{
         console.log(error);
     });
+
+app.use('/api/items', items);
 
 const port = 3000;
 
