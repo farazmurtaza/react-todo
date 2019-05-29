cc444e35501a242494106984f58996b76aaab67f
farazmurtaza <farazmemon.16@gmail.com>
2019-05-30 02:10:03 +0500

    navbar added

---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavLink,
+    NavItem,
+    Container
+} from 'reactstrap';
+
+class AppNavbar extends Component {
+    state = {
+        isOpen: false
+    }
+
+    toggle = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+
+    render() {
+        return (
+            <div>
+            <Navbar color="light" light expand="sm" className="mb-4">
+                <Container>
+                    <NavbarBrand href="/">Todo List</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle}/>
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <NavLink href="https://github.com/farazmurtaza">Github</NavLink>
+                            </NavItem> 
+                            <NavItem>
+                                <NavLink href="https://linkedin.com/in/farazmurtaza">LinkedIn</NavLink>
+                            </NavItem>    
+                        </Nav>
+                    </Collapse>
+                </Container>
+            </Navbar>
+        </div>
+        );
+        
+    }
+}
+
+export default AppNavbar;
