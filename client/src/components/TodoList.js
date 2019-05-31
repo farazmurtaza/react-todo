6eae71b4cdce09700f15ce90335b7f63c5b3bc21
farazmurtaza <farazmemon.16@gmail.com>
2019-06-01 02:30:12 +0500

    connect to MLab

--- client/src/components/TodoList.js
+++ client/src/components/TodoList.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
 import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import uuid from 'uuid';
 import { connect } from 'react-redux';
 import { getItems, deleteItem } from '../actions/itemActions';
 import PropTypes from 'prop-types';
-
-import { STATES } from 'mongoose'; //idk how this got here
 
 class TodoList extends Component {
 
@@ -27,15 +24,15 @@ class TodoList extends Component {
 
                 <ListGroup>
                     <TransitionGroup>
-                        {items.map(({ id, name })=>(
-                            <CSSTransition key={id} timeout={500} classNames="fade">
+                        {items.map(({ _id, name })=>(
+                            <CSSTransition key={_id} timeout={500} classNames="fade">
                                 <ListGroupItem>
                                     
                                     <Button
                                         className="remove-btn"
                                         color="danger"
                                         size="sm"
-                                        onClick={this.onDeleteClick.bind(this, id)}
+                                        onClick={this.onDeleteClick.bind(this, _id)}
                                     >&times;</Button>
                                     {name}
                                 </ListGroupItem>
