83f1f291789741d3ae56311e46c02001a0e4a680
farazmurtaza <farazmemon.16@gmail.com>
2019-05-31 14:35:09 +0500

    add/remove item

--- client/src/components/TodoList.js
+++ client/src/components/TodoList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import uuid from 'uuid';
+import { STATES } from 'mongoose';
 
 class TodoList extends Component {
 
@@ -31,6 +32,30 @@ class TodoList extends Component {
                         }
                     }}
                 >Add</Button>
+
+                <ListGroup>
+                    <TransitionGroup>
+                        {items.map(({ id, name })=>(
+                            <CSSTransition key={id} timeout={500} classNames="fade">
+                                <ListGroupItem>
+                                    
+                                    <Button
+                                        className="remove-btn"
+                                        color="danger"
+                                        size="sm"
+                                        onClick={()=>{
+                                            this.setState(state => ({
+                                                items: state.items.filter(item => item.id != id)
+                                            }));
+                                        }}
+                                    >&times;</Button>
+                                    {name}
+                                </ListGroupItem>
+                            </CSSTransition>
+                        ))}
+                    </TransitionGroup>
+                </ListGroup>
+
             </Container>
         );
         
